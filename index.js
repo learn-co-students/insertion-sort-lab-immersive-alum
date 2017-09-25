ca5285c744d2fea07ca7410ec696194a9cc873d9
Diljeet Singh <jeetds1994@gmail.com>
2017-09-25 15:44:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  let smallest = array[0]
+  let smallestIndex = 0
+  array.forEach((item, index) => {
+    if(smallest > item){
+      smallest = item
+      smallestIndex = index
+    }
+  })
+  array.splice(smallestIndex, 1)
+  return smallest
 }
 
 function insertionSort(array){
-
+  let sortedArr = []
+  let minVal;
+  while(array.length != 0){
+    minVal = findMinAndRemove(array)
+    sortedArr.push(minVal)
+  }
+  return sortedArr
 }
