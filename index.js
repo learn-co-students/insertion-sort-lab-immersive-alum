2d8225e8c2f6b18a48e72a8d1c68b74bb8eaa41c
Brett Schules <brettschules@gmail.com>
2017-10-13 11:00:29 -0400

    Test Passing

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function findMinAndRemove(array){
 
+function findMinAndRemove(array){
+  let minNum = array[0]
+  let index = 0
+
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] < minNum) {
+      minNum = array[i]
+      index = i
+    }
+  }
+  array.splice(index, 1)
+  return minNum
 }
 
 function insertionSort(array){
-
+  let newArray = []
+  while (array.length !==0) {
+    newArray.push(findMinAndRemove(array))
+  }
+  return newArray
 }
