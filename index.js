1f4fa1656d5036eb742cf56dc5f484ac9aca04bd
Barney Fabian <bafa@tutanota.de>
2018-07-23 07:17:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  let index
+  for (let x = 0; x < array.length; x++) {
+   if (array[x] < min) {
+     min = array[x]
+     index = x
+   } 
+  }
+  array.splice(index, 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let result = []
+  while (array.length > 0) {
+    result.push(findMinAndRemove(array))
+  }
+  return result
 }
+
