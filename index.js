7f4077ffbeedf2e0d69cf83b41d02af8ac35bd95
zfv200 <zach.vary@flatironschool.com>
2018-08-01 14:17:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
-
+  let currentMin = array[0]
+  array.forEach((element)=>{
+    if (element < currentMin){
+      currentMin = element
+    }
+  })
+  // console.log(currentMin);
+  // console.log(array.slice(array.indexOf(currentMin), array.indexOf(currentMin+1)));
+  // let minimum = array.slice(array.indexOf(currentMin), array.indexOf(currentMin)+1)[0]
+  array.splice(array.indexOf(currentMin), 1)
+  return currentMin
+  // let returnObj = [minimum, newArray]
 }
 
 function insertionSort(array){
-
+  let newArray = []
+  let length = array.length
+  for(let i = 0; i < length; i++){
+    newArray.push(findMinAndRemove(array))
+    // array = [...array.slice(0, array.indexOf(findMinAndRemove(array))), ...array.slice(array.indexOf(findMinAndRemove(array))+1)]
+  }
+  return newArray
 }
