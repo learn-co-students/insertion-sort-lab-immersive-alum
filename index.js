58524bed434534863a63cce4f1205396b5443378
matt maher <mattmaher0@gmail.com>
2018-10-23 09:52:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+let min = array[0]
+let index = 0
+for(let i = 0; i < array.length; i++) {
+  if (array[i] < min) {
+  min = array[i]
+  index = i
+  }
+}
+array.splice(index, 1)
+return min
 }
 
 function insertionSort(array){
-
+    let newMin = null
+    let sorted = []
+    while(array.length > 0){
+      newMin = findMinAndRemove(array)
+      sorted.push(newMin)
+    }
+    return sorted;
 }
