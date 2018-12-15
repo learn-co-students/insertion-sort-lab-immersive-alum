c61cbcb43a04869732b7e4f6b2add1a74a3e1ad6
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-12-14 19:57:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  for (let num of array) {
+    if (num < min) {
+      min = num
+    }
+  }
+  array.splice(array.indexOf(min), 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let newArr = []
+  while (array.length > 0) {
+    newArr.push(findMinAndRemove(array))
+  }
+  return newArr
 }
