43cf0eb968471ed04cb057385e191b5bd84c732d
Rose <rose1264@gmail.com>
2018-12-18 11:54:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  let minEle = array[0]
+  let minEleIdx = 0
+  for (let i=0; i<array.length; i++){
+    if(array[i] < minEle){
+      minEle = array[i]
+      minEleIdx = i
+    }
+  }
+  array.splice(minEleIdx, 1)
+  return minEle
 }
 
-function insertionSort(array){
-
+function insertionSort(array){ //it's still selection sort with space complexity of O(n)
+  let resultArray = []
+  while(array.length){
+    resultArray.push(findMinAndRemove(array))
+  }
+  return resultArray
 }
