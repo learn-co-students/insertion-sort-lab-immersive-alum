61244b04a4a20c8c37397f94a80e928e66e3331a
yunglleung1 <yungl.leung1@gmail.com>
2018-12-24 11:49:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function findMinAndRemove(array){
-
+  array = array.sort((a, b) => {
+    return a - b;
+  });
+  return array.shift();
 }
 
 function insertionSort(array){
+  let sortedArray = [];
+  let min;
 
+  while(array.length > 0){
+    min = findMinAndRemove(array);
+    sortedArray.push(min);
+  }
+  return sortedArray;
 }
